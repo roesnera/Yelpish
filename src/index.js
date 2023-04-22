69d3837119e1d66fc988f8ca56f8560bfc789bba
roesnera <adam.roesner@generation.org>
2023-04-22 13:57:02 -0400

    adds basic ApolloClient call, needs auth and cors headers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App.js";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
-import App from "./App";
+
+/* TODO: authentication, CORS headers on ApolloClient */
+
+const client = new ApolloClient({
+    uri: 'https://api.yelp.com/v3/graphql',
+    cache: new InMemoryCache(),
+  });
+
+
+client
+.query({
+query: gql`
+    query GetLocations {
+    locations {
+        id
+        name
+        description
+        photo
+    }
+    }
+`,
+})
+.then((result) => console.log(result));
 
 const el = document.getElementById("root");
 
