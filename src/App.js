9b7b524b1916ab4ff7ae53165783e07937c8420a
roesnera <adam.roesner@generation.org>
2023-04-22 13:30:49 -0400

    basic project setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import AnimalShow from "./AnimalShow";
+
+function App() {
+    return <div>
+        <h1>Animals will go here</h1>
+        <AnimalShow />
+        </div>;
+}
+
+export default App;
